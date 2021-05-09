351aa71a16b03b4b971f8b597131eeba1d25298a
Filip Vojtko <66070449+filipcimo@users.noreply.github.com>
2021-05-09 14:44:06 +0200

    Update CString.h

--- CString.h
+++ CString.h
@@ -13,151 +13,28 @@ struct CString
 };
 
 
-static void cstringDestructor(struct CString** const str)
-{
-  if (str != NULL && str[0] != NULL)
-  {
-    if (str[0]->text != NULL)
-    {
-      free(str[0]->text);
-    }
 
-    free(str[0]);
-    str[0] = NULL;
-  }
-}
+void cstringDestructor(struct CString** const str);
 
+void cstringUninit(struct CString* const str);
 
-static void cstringUninit(struct CString* const str)
-{
-  if (str != NULL && str->text != NULL)
-  {
-    free(str->text);
+void cstringInit(struct CString* const str);
 
-    str->text = NULL;
-    str->textSize = 0;
-  }
-}
+void cstringInitCC(struct CString* const str, const struct CString* const str2);
 
+void cstringInitStr(struct CString* const str, const char* const str2);
 
-static void cstringInit(struct CString* const str)
-{
-  str->text = NULL;
-  str->textSize = 0;
-}
+void cstringUnit(struct CString* const str, const struct CString* const str2);
 
+char* cstringData(const struct CString* const str);
 
-static void cstringInitCC(struct CString* const str, const struct CString* const str2)
-{
-  if (str != NULL && str2 != NULL && str2->text != NULL)
-  {
-    if (str->text != NULL)
-    {
-      free(str->text);
-    }
+uint64_t cstringSize(const struct CString* const str);
 
-    str->textSize = str2->textSize;
-    str->text = (char*) malloc(str->textSize);
+char cstringAt(const struct CString* const str, const uint64_t pos);
 
-    strncpy(str->text, str2->text, str->textSize);
-  }
-}
+char* cstringAtPtr(const struct CString* const str, const uint64_t pos);
 
-
-static void cstringInitStr(struct CString* const str, const char* const str2)
-{
-  if (str != NULL && str2 != NULL)
-  {
-    if (str->text != NULL)
-    {
-      free(str->text);
-    }
-
-    str->textSize = strlen(str2);
-    str->text = (char*) malloc(str->textSize);
-
-    strncpy(str->text, str2, str->textSize);
-  }
-}
-
-
-static void cstringUnit(struct CString* const str, const struct CString* const str2)
-{
-  if (str != NULL && str2 != NULL && str2->text != NULL)
-  {
-    char* newText = (char*) malloc(str->textSize + str2->textSize);
-
-    if (str->text != NULL)
-    {
-      strncpy(newText, str->text, str->textSize);
-      free(str->text);
-      str->text = NULL;
-    }
-
-    strncpy((newText + str->textSize), str2->text, str2->textSize);
-    str->text = newText;
-
-    str->textSize = str->textSize + str2->textSize;
-  }
-}
-
-
-static char* cstringData(const struct CString* const str)
-{
-  return str->text;
-}
-
-
-static uint64_t cstringSize(const struct CString* const str)
-{
-  return str->textSize;
-}
-
-
-static char cstringAt(const struct CString* const str, const uint64_t pos)
-{
-  if (pos >= str->textSize)
-  {
-    fprintf(stderr, "Error: Index out of range ");
-    fprintf(stderr, "(Index: %li)\n", pos);
-
-    free(str->text);
-
-    exit(EXIT_FAILURE);
-  }
-
-  return str->text[pos];
-}
-
-
-static char* cstringAtPtr(const struct CString* const str, const uint64_t pos)
-{
-  if (pos >= str->textSize)
-  {
-    fprintf(stderr, "Error: Index out of range ");
-    fprintf(stderr, "(Index: %li)\n", pos);
-
-    free(str->text);
-
-    exit(EXIT_FAILURE);
-  }
-
-  return (char*) &str->text[pos];
-}
-
-
-static void cstringPrint(const struct CString* const str, const char terminator)
-{
-  if (str != NULL && str->text != NULL)
-  {
-    for (uint64_t i = 0; i < str->textSize; i = i + 1)
-    {
-      printf("%c", str->text[i]);
-    }
-
-    printf("%c", terminator);
-  }
-}
+void cstringPrint(const struct CString* const str, const char terminator);
 
 
 
